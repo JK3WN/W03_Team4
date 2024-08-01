ac446523ddd87ab1242265fbdc9912667dfe9bd5
JK3WN <yimjay1@gmail.com>
2024-08-01 15:45:21 +0900

    Added Brick & Platform Moving

--- Assets/Scripts/MovePlatform.cs
+++ Assets/Scripts/MovePlatform.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class MovePlatform : MonoBehaviour
 {
+    [Header("Movement")]
+    public Vector2 MoveSpeed;
+
+    [Header("Stats")] public float MaxHP;
+    private float HP;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        HP = MaxHP;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 NewPosition = new Vector2(transform.position.x, transform.position.y) + MoveSpeed * Time.deltaTime;
+        transform.position = NewPosition;
     }
 }
