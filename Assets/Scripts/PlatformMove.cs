7e9f2c74201b9b429023a1287974401dc24340ca
JK3WN <yimjay1@gmail.com>
2024-08-01 21:55:00 +0900

    Added Auto Constraints

--- Assets/Scripts/PlatformMove.cs
+++ Assets/Scripts/PlatformMove.cs
@@ -10,18 +10,22 @@ public class PlatformMove : MonoBehaviour
     [Header("Stats")] public float MaxHP = 100f;
     private float HP;
 
+    private Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
         HP = MaxHP;
+        rb = GetComponent<Rigidbody2D>();
+        if (MoveSpeed.x == 0) rb.constraints = RigidbodyConstraints2D.FreezePositionX;
+        if (MoveSpeed.y == 0) rb.constraints = RigidbodyConstraints2D.FreezePositionY;
     }
 
     // Update is called once per frame
     void Update()
     {
         //YJK, 시간에 따라 위치를 MoveSpeed만큼 이동
-        Vector2 NewPosition = new Vector2(transform.position.x, transform.position.y) + MoveSpeed * Time.deltaTime;
-        transform.position = NewPosition;
+        rb.velocity = MoveSpeed;
 
         //YJK, HP가 0 이하가 되면 이 플랫폼 삭제
         if (HP <= 0)
