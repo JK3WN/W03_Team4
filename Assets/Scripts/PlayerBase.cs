df7f4ab547e5a44be1e416783dd37561a0fbc1a5
Wooseok Cho <crasy4368@gmail.com>
2024-08-04 12:18:44 +0900

    feat: PlayerBase Add Comment

--- Assets/Scripts/PlayerBase.cs
+++ Assets/Scripts/PlayerBase.cs
@@ -2,59 +2,88 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// <para>
+/// 작성자 : 
+/// </para>
+/// <para>
+/// ===========================================
+/// </para>
+/// 플레이어 오브젝트 관리용 클래스
+/// 플레이어에 적용될 각종 컴포넌트를 참조받아 PlayerBase에서 플레이어의 모든 물리 벡터 적용
+/// <para>
+/// FinalVector: 플레이어에 적용할 모든 물리 벡터를 합친 최종 벡터
+/// </para>
+/// </summary>
 public class PlayerBase : MonoBehaviour
 {
+    #region 인스펙터 변수 선언
+    [Header("Player Base Components")]
     [SerializeField] private Rigidbody2D rb;
-    [SerializeField] private Vector2 FinalVector2;
     [SerializeField] private HorizontalMove horizontalMove;
     [SerializeField] private PlayerCollision playerCollision;
     [SerializeField] private PlayerJump playerJump;
     [SerializeField] private PlayerWallClimb playerWallClimb;
-    private PlayerDash playerDash;
+    [SerializeField] private PlayerDash playerDash;
 
-    // Start is called before the first frame update
+    [Space]
+    [Header("Player Physics")]
+    [SerializeField] private Vector2 FinalVector;
+    #endregion
+
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        horizontalMove = GetComponent<HorizontalMove>();
+        playerCollision = GetComponent<PlayerCollision>();
+        playerJump = GetComponent<PlayerJump>();
+        playerWallClimb = GetComponent<PlayerWallClimb>();
         playerDash = GetComponent<PlayerDash>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        FinalVector2 = Vector2.zero;
+        FinalVector = Vector2.zero;
 
+        // 대쉬 시 velocity를 고정하여 적용, 대쉬 끝나기 전까지 다른 움직임 차단
         if (playerDash.IsDashing)
         {
             rb.velocity = playerDash.DashVector;
             return;
         }
 
+        // 플레이어가 벽을 잡고 있으면 매달리기와 벽점프에 대한 벡터만 적용
         if (playerWallClimb.IsWallClimbing)
         {
-            FinalVector2 += playerWallClimb.GetWallVector();
-            FinalVector2 += playerJump.GetJumpVector();
+            FinalVector += playerWallClimb.GetWallVector();
+            FinalVector += playerJump.GetJumpVector();
         }
         else
         {
-            if (GetComponent<PlayerCollision>().GroundObject != null)
+            // 붙어있는 플랫폼의 속력 추가
+            if (playerCollision.GroundObject != null)
             {
-                FinalVector2 += new Vector2(GetComponent<PlayerCollision>().GroundObject.GetComponent<Rigidbody2D>().velocity.x, 0);
+                FinalVector += new Vector2(playerCollision.GroundObject.GetComponent<Rigidbody2D>().velocity.x, 0);
             }
 
+            // 벽점프 시 벽점프 벡터 적용
             if (playerJump.HasWallJumped)
             {
-                FinalVector2 += playerJump.LerpWallJumpVector(horizontalMove.GetMoveVector());
+                FinalVector += playerJump.LerpWallJumpVector(horizontalMove.GetMoveVector());
             }
+            // 외에는 이동 벡터 적용
             else
             {
-                FinalVector2 += horizontalMove.GetMoveVector();
+                FinalVector += horizontalMove.GetMoveVector();
             }
 
-            FinalVector2 += playerJump.GetJumpVector();
-            FinalVector2 += playerJump.GetGravityVector();
+            FinalVector += playerJump.GetJumpVector();
+            FinalVector += playerJump.GetGravityVector();
         }
 
-        rb.velocity = FinalVector2;
+        // 최종 벡터 적용
+        rb.velocity = FinalVector;
     }
 }
