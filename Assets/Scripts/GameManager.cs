943d75562673aa3f8ddd30d6b36cf649e59b6f80
JK3WN <yimjay1@gmail.com>
2024-08-04 18:55:26 +0900

    Dash Bar UI Added

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public static bool isPlaying = true;
     private float startTime;
+
+    public GameObject player;
+    public GameObject[] DashGauge, DashSlots;
 
     public TMPro.TextMeshProUGUI timerText;
 
@@ -21,10 +25,21 @@ public class GameManager : MonoBehaviour
     {
         if (isPlaying)
         {
+            // YJK, 타이머 돌아가는 코드
             float timeElapsed = Time.time - startTime;
             string min = ((int)timeElapsed / 60).ToString("00");
             string sec = (timeElapsed % 60).ToString("00.00");
             timerText.text = min + ":" + sec;
+
+            // YJK, 대쉬 빈 칸은 MaxSlots에 맞게, 대쉬 게이지는 Dashes에 맞게
+            for(int i = 0; i < player.GetComponent<PlayerDash>().MaxDashes; i++)
+            {
+                if (!DashSlots[i].activeSelf) DashSlots[i].SetActive(true);
+            }
+            for(int i = 0; i < 5; i++)
+            {
+                DashGauge[i].GetComponent<Image>().fillAmount = Mathf.Clamp(player.GetComponent<PlayerDash>().Dashes - (float)i, 0f, 1f);
+            }
         }
     }
 }
